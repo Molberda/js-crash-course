6fc8c8c6e3556dd84850368c09713b0ef60c1216
David <davidmollera@hotmail.com>
2022-10-20 16:35:24 -0500

    experimented by combining more than one if else into the same conditional

--- app.js
+++ app.js
@@ -1,9 +1,19 @@
-let suscribed = true
+let suscribed = false
+let login = false
+let date = true 
 
 if (suscribed === true) {
     console.log ('show the video')
 }
 
+else if (login === true) {
+    console.log ('please go pro')
+}
+
+else if (date === true) {
+    console.log ('please check date')
+}
+
 else {
-    console.log ('please subscribe')
+    console.log ('please login')
 }
