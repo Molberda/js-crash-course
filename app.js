c65de7c86c6ddb4ec4a558d87cfa6cbcec098f11
David <davidmollera@hotmail.com>
2022-10-20 20:51:24 -0500

    learning how to use ternary operators

--- app.js
+++ app.js
@@ -1,14 +1,4 @@
-let cash = 54
+let cash = 23
 let price = 30
-let storeOpen = true
-if (cash >= price && storeOpen === true) {
-    console.log (`here you go  ${cash - price} dollars in change`)
-}
 
-else if (cash === price) {
-    console.log ('no change')
-}
-
-else {
-    console.log ('you still owe ' + (price - cash) + ' please')
-}
+cash >= price ? console.log('success') : console.log ('fail')
